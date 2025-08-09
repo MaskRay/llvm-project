5ddfb725de770ae4800e0fd6b3d37e8affd1fd2e
Fangrui Song <i@maskray.me>
2025-08-09 13:17:02 -0700

    MCAssembler: Simplify fragment relaxation
    * FT_Data: skip relaxFragment
    * Others: Call relaxFragment, which computes the old size, calls the
      relevant relaxXXX function, then compares the size.

--- llvm/include/llvm/MC/MCAssembler.h
+++ llvm/include/llvm/MC/MCAssembler.h
@@ -112,15 +112,13 @@ private:
 
   /// Perform relaxation on a single fragment.
   bool relaxFragment(MCFragment &F);
-  bool relaxInstruction(MCFragment &F);
-  bool relaxLEB(MCFragment &F);
-  bool relaxBoundaryAlign(MCBoundaryAlignFragment &BF);
-  bool relaxDwarfLineAddr(MCFragment &F);
-  bool relaxDwarfCallFrameFragment(MCFragment &F);
-  bool relaxCVInlineLineTable(MCCVInlineLineTableFragment &DF);
-  bool relaxCVDefRange(MCCVDefRangeFragment &DF);
-  bool relaxFill(MCFillFragment &F);
-  bool relaxOrg(MCOrgFragment &F);
+  void relaxInstruction(MCFragment &F);
+  void relaxLEB(MCFragment &F);
+  void relaxBoundaryAlign(MCBoundaryAlignFragment &BF);
+  void relaxDwarfLineAddr(MCFragment &F);
+  void relaxDwarfCallFrameFragment(MCFragment &F);
+  void relaxCVInlineLineTable(MCCVInlineLineTableFragment &DF);
+  void relaxCVDefRange(MCCVDefRangeFragment &DF);
 
 public:
   /// Construct a new assembler instance.
