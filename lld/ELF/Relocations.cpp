669c42403025d1c6f8e881b8d2496cac5b717570
Fangrui Song <i@maskray.me>
2026-03-01 14:54:29 -0800

    Adjust 180297: fix nested if; adjust symbol properties

--- lld/ELF/Relocations.cpp
+++ lld/ELF/Relocations.cpp
@@ -1558,6 +1558,8 @@ void elf::postScanRelocations(Ctx &ctx) {
     }
   };
 
+  ctx.target->finalizeRelocScan();
+
   GotSection *got = ctx.in.got.get();
   if (ctx.needsTlsLd.load(std::memory_order_relaxed) && got->addTlsIndex()) {
     if (ctx.arg.shared)
